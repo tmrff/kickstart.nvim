3afd1b7d5dc4b263d3b2b963c06176eaae612ee7
tmrff <thomasdroff@gmail.com>
2024-10-07 22:36:02 +1300

    Fix catppuccin github path

--- init.lua
+++ init.lua
@@ -743,7 +743,7 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim.nvim',
+    'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
